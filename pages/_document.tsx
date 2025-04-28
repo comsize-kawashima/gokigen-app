ac8c3483bfa8b0fcb2d7ec8053bf5a3abc09c3ca
comsize-kawahima <naoya.kawashima@comsize.com>
2025-04-28 18:12:45 +0900

    [update]_document.tsxのコメントを追加し、コードの可読性を向上

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,21 +2,26 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
+    // HTML ドキュメントのルート要素
     <Html lang="ja">
       <Head>
+        {/* Google Fonts の事前接続設定 */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="anonymous"
         />
+        {/* Nico Moji フォントを読み込むリンク */}
         <link
           href="https://fonts.googleapis.com/earlyaccess/nicomoji.css"
           rel="stylesheet"
         />
       </Head>
       <body className="antialiased">
+        {/* ページのメインコンテンツを表示 */}
         <Main />
+        {/* Next.js のスクリプトを挿入 */}
         <NextScript />
       </body>
     </Html>
